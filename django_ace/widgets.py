09048a870aea8e0dcf86b3d1fa07a25bdad3285f
Alexander <xranux@gmail.com>
2022-07-08 16:38:40 +0300

    fix: error

--- django_ace/widgets.py
+++ django_ace/widgets.py
@@ -27,7 +27,7 @@ class AceWidget(forms.Textarea):
         readonly=False,
         showgutter=True,
         behaviours=True,
-        self.emmet = False,
+        emmet = False,
         *args,
         **kwargs
     ):
